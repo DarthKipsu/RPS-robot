dbfe82936acfa686fc17864e2d8ac2ff26353b11
Verna Koskinen <darth.kipsu@gmail.com>
2015-12-23 16:05:18 +0200

    add perceptron algorithm

--- MachineLearning/load_data.py
+++ MachineLearning/load_data.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 def read_training_data():
+    """
+    Reads training data and labels created by Labeler in RPCRunner.
+    :return: tuple (X, y) where X is list of the 650 bits training images and y
+    the corresponding labels {0, 1, 2}
+    """
     data_file = open('../RPCRunner/data/data', 'rb')
     labels_file = open('../RPCRunner/data/labels', 'rb')
     labels = np.loadtxt(labels_file, dtype=np.int8)
-    data = np.fromstring(np.array([data_file.read(650) for i in labels]), dtype=np.uint8)
-    return (np.reshape(data, (-1, 650)), labels)
-
-print(read_training_data())
+    data = np.fromstring(np.array([data_file.read(650) for i in labels]),
+            dtype=np.uint8)
+    return np.reshape(data, (-1, 650)), labels
---
+++ MachineLearning/perceptron.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+epoch_count = 10
+
+def perceptron(data, label):
+    """
+    A perceptron algorithm for training a multiclass linear classifier, using
+    the pocket modification to be able to use with data not linearry seperable.
+    :input: training data and corresponding labels where + sign data items have
+    label +1 and - sign data items label -1
+    :return: weight vector w defining pred_y = sign(w * x)
+    """
+    w = np.zeros(len(data[0]), np.int32)
+    pocket = np.copy(w)
+    pocket_count = 0
+    for epoch in range(epoch_count):
+        converged = True
+        correct_count = 0
+        for i in range(len(data)):
+            prediction = np.sign(w.dot(data[i]))
+            if prediction != label[i]:
+                if correct_count > pocket_count:
+                    pocket = np.copy(w)
+                    pocket_count = correct_count
+                correct_count = 0
+                w += label[i] * data[i]
+                converged = False
+            else:
+                correct_count += 1
+        if converged:
+            return w
+    if pocket_count == 0:
+        print 'Something not right: 0 pockets'
+        return w
+    return pocket
