3a3317efc0a13a7a2105237bf4abe720554bae5b
Verna Koskinen <darth.kipsu@gmail.com>
2015-12-28 15:28:08 +0200

    refactor GameDisplay

--- RPCRunner/src/rpcrunner/GameDisplay.java
+++ RPCRunner/src/rpcrunner/GameDisplay.java
@@ -1,6 +1,9 @@
 
 package rpcrunner;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -16,24 +19,20 @@ import javafx.util.Duration;
  * be playing.
  */
 public class GameDisplay {
+    private final String COUNT_TEXT = "Let's start by counting down from 3. On "
+            + "zero, reveal your rock, paper or scissors.";
+    
     private Stage stage;
-    private String opponent;
 
     public GridPane gameGridPane(Stage stage, String opponent) {
         this.stage = stage;
-        this.opponent = opponent;
+        String welcome = "Welcome " + opponent + ". I want to play a game.";
         GridPane grid = rpcGrid();
-        grid.add(new Text("Welcome " + opponent + ". I want to play a game."), 0, 0);
-        grid.add(new Text("Let's start by counting down from 3. On zero, reveal your rock, paper or scissors."), 0, 1);
+        grid.add(new Text(welcome), 0, 0);
+        grid.add(new Text(COUNT_TEXT), 0, 1);
         Text countDown = new Text("");
         grid.add(countDown, 0, 2);
-        
-        Timeline timeline = new Timeline();
-        timeline.getKeyFrames().add(countFor(5, "3", countDown));
-        timeline.getKeyFrames().add(countFor(6, "2", countDown));
-        timeline.getKeyFrames().add(countFor(7, "1", countDown));
-        timeline.getKeyFrames().add(countFor(8, "Play!", countDown));
-        timeline.play();
+        addTimelineEffects(countDown);
         return grid;
     }
 
@@ -45,12 +44,36 @@ public class GameDisplay {
         return grid;
     }
 
+    private void addTimelineEffects(Text countDown) {
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(countFor(8, "3", countDown));
+        timeline.getKeyFrames().add(countFor(9, "2", countDown));
+        timeline.getKeyFrames().add(countFor(10, "1", countDown));
+        timeline.getKeyFrames().add(countFor(11, "Play!", countDown));
+        timeline.getKeyFrames().add(closeAfter(12));
+        timeline.play();
+    }
+
     private KeyFrame countFor(int duration, String count, Text countDown) {
         return new KeyFrame(Duration.seconds(duration),
             new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
                     countDown.setText(count);
+                }
+            });
+    }
+
+    private KeyFrame closeAfter(int duration) {
+        return new KeyFrame(Duration.seconds(duration),
+            new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    try {
+                        stage.close();
+                        RPCRunner.displayResults(stage);
+                    } catch (IOException | InterruptedException ex) {
+                    }
                 }
             });
     }
